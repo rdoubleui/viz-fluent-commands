c12beb0655b7d0936bf9865a995a8f6262bd13ea
Rajko Winkler <rdoubleui@users.noreply.github.com>
2015-10-14 23:32:53 +0200

    Update app.js added comments

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var viz = require("viz-fluent-commands");
 
+// get an instance of the command builder
 var cmd_builder = viz.create();
 
+// add a layer defined in 'constants'
 cmd_builder.addLayer(viz.constants().main_layer);
-
 console.log('Layer set: ' + cmd_builder.layer);
