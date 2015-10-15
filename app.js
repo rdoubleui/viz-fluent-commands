ebe77a7c30beaf38c66d933f90a8a39dbb7df6d4
Rajko Winkler <rajko.winkler@gmail.com>
2015-10-15 22:11:39 +0200

    Added prototype method for setting the layer

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var viz = require("viz-fluent-commands");
 
 // get an instance of the command builder
-var cmd_builder = viz.create();
+var cmd = viz.create();
 
 // add a layer defined in 'constants'
-cmd_builder.addLayer(viz.constants().main_layer);
-console.log('Layer set: ' + cmd_builder.layer);
+cmd.setLayer(viz.const.MAIN_LAYER);
+console.log('Layer set: ' + cmd.layer);
