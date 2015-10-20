1f6c5fb915393ecb6d844d026e9ab457c43c4bee
Rajko Winkler <rajko.winkler@gmail.com>
2015-10-20 11:58:51 +0200

    added more sample code

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ var viz = require("viz-fluent-commands");
 var cmd = viz.create();
 
 // add a layer defined in 'constants'
-cmd.setLayer(viz.const.MAIN_LAYER).setLayer(viz.const.FRONT_LAYER);
-console.log('Layer set: ' + cmd.layer);
+var str = cmd.setLayer(viz.const.MAIN_LAYER).setLayer(viz.const.FRONT_LAYER).getCommandString();
+console.log(str);
+
+str = cmd.setLayer(viz.const.MAIN_LAYER).setControlContainer('control2').getCommandString();
+console.log(str);
+
+console.log(cmd._layer);
