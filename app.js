8c1e665d0dee68e14d90dcecf99767b5d3e46f81
Rajko Winkler <rajko.winkler@gmail.com>
2015-11-03 16:23:12 +0100

    changed returned module to closure

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var viz = require("viz-fluent-commands");
 var cmd = viz.create();
 var cmd2 = viz.create();
 
-console.log(cmd.setLayer(cmd.getConstants().MAIN_LAYER).getCommandString());
-console.log(cmd2.setLayer(cmd.getConstants().MAIN_LAYER).setControlContainer('control2').getCommandString());
-console.log(cmd.setLayer(cmd.getConstants().FRONT_LAYER).setControlContainer('scripting').getCommandString());
+console.log("CONSTANTS: " + viz.layers.MAIN_LAYER);
+
+console.log(cmd.setLayer(viz.layers.MAIN_LAYER).getCommandString());
+console.log(cmd2.setLayer(viz.layers.MAIN_LAYER).setControlContainer('control1').setControlContainer('control2').getCommandString());
+console.log(cmd.setLayer(viz.layers.FRONT_LAYER).setControlContainer('scripting').getCommandString());
 
 // the layer is not accessible from here
 console.log(cmd._layer);
