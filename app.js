61e8d1d11d1d6cb961f9011a720b2a27469e0673
Rajko Winkler <rajko.winkler@gmail.com>
2015-11-03 16:26:12 +0100

    changed name of factory method

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var viz = require("viz-fluent-commands");
 
 // get an instance of the command builder
-var cmd = viz.create();
-var cmd2 = viz.create();
+var cmd1 = viz.createCommand();
+var cmd2 = viz.createCommand();
 
 console.log("CONSTANTS: " + viz.layers.MAIN_LAYER);
 
-console.log(cmd.setLayer(viz.layers.MAIN_LAYER).getCommandString());
-console.log(cmd2.setLayer(viz.layers.MAIN_LAYER).setControlContainer('control1').setControlContainer('control2').getCommandString());
-console.log(cmd.setLayer(viz.layers.FRONT_LAYER).setControlContainer('scripting').getCommandString());
+console.log(cmd1.setLayer(viz.layers.MAIN_LAYER).getCommandString());
+console.log(cmd2.setLayer(viz.layers.MAIN_LAYER).setLayer(viz.layers.FRONT_LAYER).setControlContainer('control1').setControlContainer('control2').getCommandString());
+console.log(cmd1.setLayer(viz.layers.FRONT_LAYER).setControlContainer('scripting').getCommandString());
 
 // the layer is not accessible from here
-console.log(cmd._layer);
+console.log(cmd1._layer);
